2d4b45d3f53cb8b2ebb51a684830907d4ec807c7
Pablo Archenti <pabloarchenti@cookunity.com>
2022-08-31 12:26:33 -0300

    Styling

---
+++ app/src/components/styled/Container.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ContainerTitle = styled.div`
+  margin: 15px;
+  .title {
+    font-weight: bold;
+  }
+  .bt {
+    text-align: right;
+  }
+`;
